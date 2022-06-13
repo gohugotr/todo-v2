60cd503800c861fcdacec0508325e22342328849
Cengiz YILMAZ <terabilgi@gmail.com>
2022-06-13 13:03:11 +0300

    autofocus eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Input from './components/Input'
 import Header from "./components/Header"
 
 const App = () => {
 
-
   return (
-    <div className="md:container md:mx-auto">
-        <Header/>
+    <div className='md:container md:mx-auto'>
+      <Header>
+        <Input />
+      </Header>
     </div>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+const Input = () => {
+  return (
+    <div className='w-full mt-4'>
+      <input
+        className='w-full rounded border-none outline-none text-gray-600 p-2 font-semibold'
+        placeholder='Yeni TODO oluştur' autoFocus/>
+    </div>
+  )
+}
+
+export default Input
